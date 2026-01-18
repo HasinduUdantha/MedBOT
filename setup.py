f52ae3493f5ccbe54bb79bd02cba83dd40a0e836
Hasindu Udantha <hasinduudantha21@gmail.com>
2026-01-18 17:09:16 +0530

    add initial setup and requirements files

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="MedBOT",
+    version="0.1.0",
+    author="HasinduUdanta",
+    author_email="hasinduudantha21@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
